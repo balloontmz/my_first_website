72bf866c6cbb609ccf9939c715338022de2fb355
tomtiddler <15111171986@163.com>
2018-08-02 01:43:33 +0800

    handlers update

--- www/handlers.py
+++ www/handlers.py
@@ -8,18 +8,53 @@ handlers
 __author__ = 'tomtiddler'
 
 from www.coroweb import get, post
-from www.models import User, Blog, next_id
+from www.models import User, Blog, Comment, next_id
 from www.apis import APIError, APIPermissionError, APIResourceNotFoundError, APIValueError
 from www.config import configs
-import time, re, hashlib, json
+import time, re, hashlib, json, logging, asyncio
 from aiohttp import web
 
 COOKIE_NAME = 'websession'
 _COOKIE_KEY = configs.session.secret
 
 
-def user2cookie():
-    pass
+def user2cookie(user, max_age):
+    '''
+    Generate cookie str by user
+    '''
+    # build cookie string by: id-expires-sha1
+    expires = str(int(time.time() + max_age))  # 过期时间
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)  # 组合cookie, cookie_key 取自配置文件
+    ls = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
+    return '-'.join(ls)
+
+
+# 本函数暂时用于auth_factory 验证登录用户
+async def cookie2user(cookie_str):
+    '''
+    Parse cookie and load user if cookie is valid
+    '''
+    if not cookie_str:
+        return None
+    try:
+        L = cookie_str.split('-')
+        if len(L) != 3:
+            return None
+        uid, expires, sha1 = L
+        if int(expires) < time.time():
+            return None
+        user = await User.find(uid)
+        if user is None:
+            return None
+        s = '%s-%s-%s-%s' % (uid, user.passwd, expires, _COOKIE_KEY)
+        if sha1 != hashlib.sha1(s.encode('utf-8')).hexdigest():
+            logging.info('invalid sha1')
+            return None
+        user.passwd = '******'
+        return user
+    except Exception as e:
+        logging.exception(e)
+        return None
 
 
 @get('/')
@@ -32,10 +67,35 @@ def index(request):
     ]
     return {
         '__template__': 'blogs.html',
-        'blogs': blogs
+        'blogs': blogs,
+        '__user__': request.__user__  # 解决无法显示已登录用户的问题
     }
 
 
+@get('/register')
+def register():
+    return {
+        '__template__': 'register.html'
+    }
+
+
+@get('/signin')
+def signin():
+    return {
+        '__template__': 'signin.html'
+    }
+
+
+@get('/signout')  # 这个URL处理函数有通过中间件吗?
+def signout(request):
+    referer = request.headers.get('Referer')
+    r = web.HTTPFound(referer or '/')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly=True)
+    logging.info('user signed out.')
+    return r
+
+
+'''
 # 第一个api
 @get('/api/users')
 async def api_get_users():
@@ -43,14 +103,14 @@ async def api_get_users():
     for u in users:
         u.password = '******'
     return dict(users=users)
+'''
 
-
-_RE_EMAIL = re.compile(
-    r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')  # 正则表达式，由于浏览器端已经把email地址小写化了，所以不需要匹配大写
+# 正则表达式，由于浏览器端已经把email地址小写化了，所以不需要匹配大写
+_RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')  # 16进制字符串？
 
 
-@post('/api/users')
+@post('/api/users')  # 用于注册
 async def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
         raise APIValueError('name')
@@ -62,12 +122,38 @@ async def api_register_user(*, email, name, passwd):
     if len(users) > 0:
         raise APIError('register:failed', 'email', 'Email is already in use.')
     uid = next_id()
-    sha1_password = '%s:%s' % (uid, passwd)
+    sha1_password = '%s:%s' % (uid, passwd)  # passwd 是客户端经过计算后的口令，不是原始口令
     user = User(id=uid, name=name.strip(), email=email, passwd=hashlib.sha1(sha1_password.encode('utf-8')).hexdigest(),
                 image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(
                     email.encode('utf-8')).hexdigest())  # 关于图片不甚了解
     await user.save()
     # make session cookie:
+    r = web.Response()
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
+    user.passwd = '******'  # 其实密码已经经过了二次sha1计算了
+    r.content_type = 'application/json'
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')  # 关于json的序列化操作
+    return r
+
+
+@post('/api/authenticate')  # 用于登录
+async def authenticate(*, email, passwd):
+    if not email:
+        raise APIValueError('email', 'Invalid email.')
+    if not passwd:
+        raise APIValueError('passwd', 'Invalid passwd')
+    users = await User.findAll('email=?', [email])
+    if len(users) == 0:
+        raise APIValueError('email', 'Email not exist.')
+    user = users[0]
+    #  check passwd:
+    sha1 = hashlib.sha1()
+    sha1.update(user.id.encode('utf-8'))
+    sha1.update(b':')
+    sha1.update(passwd.encode('utf-8'))
+    if user.passwd != sha1.hexdigest():
+        raise APIValueError('passwd', 'Invalid password.')
+    # authenticate ok ,set cookie 跟注册一样传输JSON COOKIE
     r = web.Response()
     r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly=True)
     user.passwd = '******'  # 其实密码已经经过了二次sha1计算了
